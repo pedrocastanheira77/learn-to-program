6fb5150ddf6f0f17b655854e9fd99b3d8f71d123
Pedro Castanheira <josepedrocastanheira@gmail.com>
2016-12-02 10:49:18 +0000

    chapter 6 exercises

---
+++ chapter06/ex1.rb
@@ -0,0 +1,3 @@
+puts "HOW CAN I NOT HELP YOU?"
+answer = gets.chomp
+puts "WHADDAYA MEAN " + "\"#{answer.upcase}\"" + "?!? YOU'RE FIRED!!"
---
+++ chapter06/ex2.rb
@@ -0,0 +1,8 @@
+title = "Table of Contents".center(40)
+chapter1 = "Chapter 1: Getting Started".ljust(30) + "page 1".rjust(10)
+chapter2 = "Chapter 2: Numbers".ljust(30) + "page 9".rjust(10)
+
+puts title
+puts
+puts chapter1
+puts chapter2
