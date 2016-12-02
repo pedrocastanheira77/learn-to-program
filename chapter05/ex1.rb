4eb5c503cfbe621e27edeb50e3ea40f6d3812b89
Pedro Castanheira <josepedrocastanheira@gmail.com>
2016-12-02 10:23:55 +0000

    chapter 5 exercises

---
+++ chapter05/ex1.rb
@@ -0,0 +1,7 @@
+puts "What\'s your first name"
+first = gets.chomp
+puts "What\'s your middle name"
+middle = gets.chomp
+puts "What\'s your last name"
+last = gets.chomp
+puts "Hello #{first} #{middle} #{last}"
---
+++ chapter05/ex2.rb
@@ -0,0 +1,3 @@
+puts "What´s your favorite number"
+favorite = gets.chomp
+puts "Why not " + (favorite.to_i + 1).to_s + "? Is bigger and better number the one you choose!"
